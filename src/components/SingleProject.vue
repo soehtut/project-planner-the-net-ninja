45cf7c12a3703a34de7b3e79081d4cef38299fdd
SoeHtut <soehtut@escapepixel.com>
2021-01-18 21:38:47 +0630

    Completing Projects

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="project">
+    <div class="project" :class="{complete: project.complete}">
         <div class="actions">
             <h3 @click="showDetails = !showDetails">{{ project.title }}</h3>
             <div class="icons">
                 <span class="material-icons">edit</span>
                 <span class="material-icons" @click="delete">delete</span>
-                <span class="material-icons">done</span>
+                <span class="material-icons tick" @click="toggleComplete">done</span>
             </div>
         </div>
         <div v-if="showDetails" class="details">
@@ -27,6 +27,15 @@ export default {
         delete() {
             fetch(this.uri, {method: 'DELETE'})
             .then(() => this.$emit('delete', this.project.id))
+        },
+        toggleComplete() {
+            fetch(this.uri, {
+                method: 'PATCH',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({complete: !this.project.complete})
+            }).then(() => {
+                this.$emit('complete', this.project.id)
+            }).catch(err => console.log(err))
         }
     }
 }
@@ -40,6 +49,12 @@ export default {
     border-radius: 4px;
     box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.05);
     border-left: 4px solid #e90074;
+}
+.project.complete {
+    border-left: 4px solid #00ce89;
+}
+.project.complete .tick {
+    color: #00ce89; 
 }
 h3 {
     cursor: pointer;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <div v-if="projects.length">
       <div v-for="project in projects" :key="project.id">
-        <SingleProject :project="project" @delete="handleDelete"></SingleProject>
+        <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"></SingleProject>
       </div>
     </div>
   </div>
@@ -32,6 +32,12 @@ export default {
       this.projects = this.projects.filter(project => {
         return project.id != id
       })
+    },
+    handleComplete(id) {
+      let p = this.projects.find(project => {
+        return project.id === id
+      })
+      p.complete = !p.complete
     }
   }
 }
