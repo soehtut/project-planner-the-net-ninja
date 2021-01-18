45aa2dbb8b2a4d6ac9367fa83a9e6460007b8c6b
SoeHtut <soehtut@escapepixel.com>
2021-01-18 23:05:53 +0630

    Edit Project Route

---
+++ src/views/EditProject.vue
@@ -0,0 +1,30 @@
+<template>
+    <form>
+        <label>Title</label>
+        <input type="text" v-model="title" required>
+        <label>Details</label>
+        <input type="text" v-model="details" required>
+        <button>Update Project</button>
+    </form>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    data() {
+        return {
+            title: '',
+            details: '',
+            uri: 'http://localhost:3000/projects/' + this.id
+        }
+    },
+    mounted() {
+        fetch(this.uri)
+        .then(res => res.json())
+        .then(data => {
+            this.title = data.title
+            this.details = data.details
+        }).catch(err => console.log(err))
+    }
+}
+</script>
